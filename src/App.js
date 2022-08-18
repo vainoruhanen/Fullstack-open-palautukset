13952abdb54824887074c0d1d11c5a7ac6ca8df1
Ruhanen <vaino.ruhanen@itvelhot.local>
2022-08-18 12:15:38 +0300

    tehtävä 1.5 tehty

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Header = (props) =>{
+  return(
+    <h1>{props.title}</h1>
+  )
 }
 
-export default App;
+const Total = (props) =>{
+    return(
+      <p>Number of exercises {props.taulukko[0].exercises + props.taulukko[1].exercises + props.taulukko[2].exercises}</p>
+    )
+}
+
+const Content = (props) =>{
+    return(
+      <div>
+      <Part name = {props.taulukko[0].name} amount = {props.taulukko[0].exercises}></Part>
+      <Part name = {props.taulukko[1].name} amount = {props.taulukko[1].exercises}></Part>
+      <Part name = {props.taulukko[2].name} amount = {props.taulukko[2].exercises}></Part>
+      </div>
+    )
+}
+
+
+const Part = (props) =>{
+  
+  return(
+    <div>
+      <p>{props.name} {props.amount}</p>
+    </div>   
+    )
+}
+
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+
+    <div>
+      <Header title = {course.name}></Header>
+
+      <Content taulukko = {course.parts}></Content>
+      
+     <Total taulukko = {course.parts}></Total>
+    </div>
+  )
+}
+
+export default App
