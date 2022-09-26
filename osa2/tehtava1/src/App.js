6099bf9b109645a30eb4b786589d10940b73df22
Ruhanen <vaino.ruhanen@itvelhot.local>
2022-09-26 13:07:50 +0300

    ohjelma toimii useammalla kurssila

--- osa2/tehtava1/src/App.js
+++ osa2/tehtava1/src/App.js
@@ -55,32 +55,66 @@ const Course = (props) => {
   )
 }
 
+
+const Courses = (props) =>{
+    const {courses} = props
+
+  return(
+    courses.map((course) =>
+    <li key = {course.id}><Course course = {course}></Course></li>
+    )
+  )
+}
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course}/>
+      <Courses courses={courses}/>
     </div>
   )
 }
